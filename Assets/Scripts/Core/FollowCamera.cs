b56278adec71937d936b0f20edb46276b11831ef
SergeiGuriev <sergei.guriev.1234@gmail.com>
2021-06-05 19:43:21 +0300

    First commit and push.

---
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] Transform target = null;
+
+        // когда в игре будет много всего то может возникнуть глюк когда камера пытается двигаться раньше(в некоторых фреймах) чем движется игрок / фрейм  
+        // движения камеры срабатывает раньше чем фрейм анимации движения. Игрока будет как будто дёргать туда - сюда.
+        // нужен будет LateUpdate, а не Update
+        void Update()
+        {
+            transform.position = target.position;
+        }
+    }
+
+    // позиция FollowCameraEmpty_GameObject'a(коробки) = позиции игрока, но внутри коробки стоит камера которая отдалена от нулевой позиции коробки/игрока 
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,48 @@
+﻿using RPG.Combat;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        NavMeshAgent navMeshAgent;
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        void UpdateAnimator()
+        {                                                                                // velocity = скорость отдаления от нулевой z координаты
+            Vector3 globalVelocity = navMeshAgent.velocity;                              // скорость движения относительно (0,0,0) мировых координат
+            Vector3 localVelocity = transform.InverseTransformDirection(globalVelocity); // если персонаж будет бежать в сторону отдаления от мировой Z координаты
+                                                                                         //Debug.Log(globalVelocity.z);                                               // то его velocity будет отрицательным
+            float speed = localVelocity.z;                                               // поэтому нужно расчитывать скорость относительно (0,0,0) координат персонажа
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);  // мне нужно знать с какой скоростью(изменения Z координаты) персонаж отдаляется от своей позиции, 
+        }                                                              // а не относительно нуля мира
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().CancelAttack();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void StopMoving()
+        {
+            navMeshAgent.isStopped = true;
+        }
+    }
+}
